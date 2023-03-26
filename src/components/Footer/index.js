f3c18d2082dd40e1ee1ee8be9f212fd5088ec771
mojsun <m.khorashahi7@gmail.com>
2023-03-26 18:40:31 -0400

    modifying the footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,21 +5,10 @@ function Footer() {
     <footer>
       <h2>Made by:</h2>
       <ul>
-        <li class="information">6475647494</li>
         <li class="information">m.khorashahi7@gmail.com</li>
         <li>
           <a href="https://github.com/mojsun/Mini-challenge" class="info">
             Github
-          </a>
-        </li>
-        <li>
-          <a href="#" class="info">
-            Twitter
-          </a>
-        </li>
-        <li>
-          <a href="#" class="info">
-            Spotify
           </a>
         </li>
       </ul>
