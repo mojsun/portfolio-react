dd8dd7fefd5975e6aca0fa60ac93aa38d25db221
mojsun <m.khorashahi7@gmail.com>
2023-03-26 20:17:11 -0400

    changing the form in contact me

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Contact() {
   return (
-    <form>
+    <form className="form">
       <h1>Contact Me</h1>
       <div class="form-group">
         <label for="emailInput">Email address</label>
